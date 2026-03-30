7dca158008e47c68fdd95f5a5bcda89fc8419a72
himanshu nagar <himanshunagar2525@gmail.com>
2026-03-30 14:08:34 +0530

    updated project page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home />}/>
       </Routes>
       <Footer />
     </div>
